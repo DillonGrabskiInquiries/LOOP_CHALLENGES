c1308684350822c9eba0c5a4a561ffe6f9609987
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-05-24 00:23:20 -0500

    ya

---
+++ loopchallenges.js
@@ -0,0 +1,43 @@
+
+// Loop 1.
+
+// for (i = 1; i < 20; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i)
+//     }
+// }
+
+// Loop 2.
+
+// for (i = 100; i > 0; i--) {
+//     if (i % 3) {
+//         console.log("nope")
+//     }
+//     else if (i % 3 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// Loop 3.
+
+// for(i = 4; i > -5; i = i - 1.5) {
+// console.log(i)
+// }
+
+// Loop 4.
+
+// var sum = 0
+// for (i = 0; i <= 100; i++) {
+//     sum = i + sum
+//     console.log(i)
+//     console.log(sum)
+// }
+
+// Loop 5.
+
+// var product = 1
+// for (i = 1; i <= 12; i++) {
+//     product = i * product
+//     console.log(i)
+//     console.log(product)
+// }
